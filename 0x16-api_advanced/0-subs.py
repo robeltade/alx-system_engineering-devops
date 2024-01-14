a933f43cce8d55ab4198633bf4e7b692ba521a63
robel tadesse <robeltadesse37@gmail.com>
2024-01-15 03:54:58 +1200

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-'''
-Returns the number of subscribers for a give subrddit
-'''
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''
-    Return number of subreddit subscribers
-    '''
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = 'reddit_user'
-
-    headers = {'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-
-    req = requests.get(url, headers=headers, allow_redirects=False)
-
-    if req.status_code == 404:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    result = response.json().get("data")
-
-    return result.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-'''
- Prints the titles of the first 10 hot
- posts listed for a given subreddit.
-'''
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    '''
-    Top 10 posts  in subreddit
-    '''
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    user_agent = 'reddit_user'
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    headers = {'User-Agent': user_agent}
-
-    req = requests.get(url, headers=headers, allow_redirects=False)
-
-    if req.status_code != 200:
-        print('None')
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
     else:
-        data = req.json()['data']
-        post_list = data['children']
-
-        for posts in post_list[0:10]:
-            print(posts['data']['title'])
+        print(None)
